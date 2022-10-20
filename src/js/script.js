38fa35ea7e779e346d39cf62f4dff0d21153965d
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-10-20 12:39:59 +0200

    Fix npm run watch

--- src/js/script.js
+++ src/js/script.js
@@ -53,18 +53,17 @@
   };
   class Product {
     constructor(id, data) {
-      const thisProduct = this;
-      
+      const thisProduct = this;      
       thisProduct.id = id;
       thisProduct.data = data;
+
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
       thisProduct.processOrder();
-
-      console.log('new Product:', thisProduct);
     }
+
     renderInMenu() {
       const thisProduct = this;
 
@@ -112,10 +111,10 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
     initOrderForm(){
       const thisProduct = this;
-      console.log('initOrderForm:' + thisProduct);
-
+      
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
         thisProduct.processOrder();
@@ -138,7 +137,6 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -147,22 +145,47 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if (optionSelected) {
+
+            // check if the option is not default
+            if (option.default != true) {
+
+              // add option price to price variable
+              price += option.price;
+            }
+            // check if the option is default
+          } else if (option.default == true) {
+
+            // deduct option price from a price variable
+            price -= option.price;
+          }
+
+          // find img with class paramId-optionId in img wrapper
+          const optionImg = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
+          if (optionImg) {
+            if (optionSelected) {
+              optionImg.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImg.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
+        }
+      }
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
-    // update calculated price in the HTML
-     thisProduct.priceElem.innerHTML = price;
-  }
-
-    }
-  }
 
   const app = {
     initMenu: function(){

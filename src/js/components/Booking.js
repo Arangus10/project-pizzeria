6f26e1701c7c282e63a2cbc603218467f83a78b0
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-11-24 14:04:34 +0100

    Remove unuseful files from vendor

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,6 @@
 import { templates, select} from './../settings.js';
 import AmountWidget from './AmountWidget.js';
-import utils from '../utils.js';
+
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
